4079c906a559a4f4207f0f1b879b1ce2ee02906c
Kawa <victor.borruat@hotmail.com>
2021-03-04 17:06:41 +0100

    added init file

---
+++ webapp/__init__.py
@@ -0,0 +1,16 @@
+# where we initialize app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__) #just the name of the module
+app.config['SECRET_KEY'] = '994fc53fc5ec59d69c69255d31fb4c72'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # relative path rfom current file -> in project directory
+db = SQLAlchemy(app) # makes DataBase
+bcrypt = Bcrypt(app)
+loginManager = LoginManager(app)
+loginManager.login_view = 'login'
+loginManager.login_message_category = 'info'
+
+from flaskblog import routes # avoids circular importing
